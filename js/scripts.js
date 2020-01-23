1bc25e4c4173353662c7a5bf031c6447cec4796c
Student <example@example.com>
2020-01-22 16:47:55 -0800

    add some logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+function sum(total, num) {
+  return total + num;
+}
+
 $(document).ready(function(){
 
   $("#start").click(function(event){
@@ -6,26 +10,36 @@ $(document).ready(function(){
     $("h1").show();
     $("#start").hide();
   });
-  //var numbers = [];
-
-
-  var lucky = 0;
-  var unlucky = 0;
+  
+  var numbers = [];
+  var counter = 1;
   $(".answer").click(function(){
 
-    console.log($(this).val());
+    var answer = ($(this).val());
+    numbers.push(answer);
+
+    if (counter < 5){
+    $("#q" + questionNumber).hide();
+    // questionNumber++;
+    // $("#q" + questionNumber).show();
+    } else {
+     var total = numbers.reduce(sum);
+     if total >= 4 {
+       console.log("you are unlucky");
+     } else if total <= 1 {
+       console.log("you are lucky!");
+     } else {
+       console.log("you are neither lucky nor unlucky")
+     }
+    }
+  });
+
+});
+
+  
     // var luck = $(this);
     // if (luck === "lucky"){
     //   lucky++;
     // } else {
     //   unlucky++;
     // }
-    // $("#q" + questionNumber).hide();
-    // questionNumber++;
-    // $("#q" + questionNumber).show();
-  
-  });
-
-});
-
-  
